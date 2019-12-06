dbc5950cba1d2d8c228a4477f2d6b1cc93170189
Brandon <brandon@modemtones.com>
2019-12-05 16:20:24 -0800

    day 2 complete

--- day01/index.js
+++ day01/index.js
@@ -1,23 +1,28 @@
 const fs = require('fs');
 
 // load file and store values in an array
-const massArray = fs.readFileSync('input.txt').toString().split("\n");
+const massArray = fs
+  .readFileSync('input.txt')
+  .toString()
+  .split('\n');
 
 let i;
 let totalFuelRequired = 0;
 
-const calculateFuel = function (mass) {
+const calculateFuel = mass => {
   // recursive function to calculate the fuel needed per part
   const fuel = Math.floor(mass / 3) - 2;
   if (fuel <= 0) return mass;
   return mass + calculateFuel(fuel);
-}
+};
 
-for(i = 0; i < massArray.length; i++) {
+// eslint-disable-next-line no-plusplus
+for (i = 0; i < massArray.length; i++) {
   // convert the mass to an integer before passing to the function
-  const partMass = parseInt(massArray[i]);
+  const partMass = parseInt(massArray[i], 10);
   // calculate fuel required and add to the sum
   totalFuelRequired += calculateFuel(partMass) - partMass;
 }
 
+// eslint-disable-next-line no-console
 console.log(`Total Fuel Required: ${totalFuelRequired}`);
---
+++ day02/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+// load file and store values in an array
+const opcodeArray = fs
+  .readFileSync('input.txt')
+  .toString()
+  .split(',');
+
+let i;
+let noun;
+let verb;
+let computationComplete = false;
+
+const processOpcode = (oc1, oc2, oc3) => {
+  return oc1 === 1 ? oc2 + oc3 : oc2 * oc3;
+};
+
+for (noun = 0; noun <= 99 && !computationComplete; noun++) {
+  for (verb = 0; verb <= 99 && !computationComplete; verb++) {
+    const testArray = [...opcodeArray];
+    let haltComputer = false;
+    testArray[1] = noun;
+    testArray[2] = verb;
+
+    for (i = 0; i <= testArray.length - 4 && !haltComputer; i += 4) {
+      const opInstruction = parseInt(testArray[i], 10);
+
+      if (opInstruction !== 99) {
+        const opParameter1 = parseInt(testArray[testArray[i + 1]], 10);
+        const opParameter2 = parseInt(testArray[testArray[i + 2]], 10);
+        const opParameter3 = parseInt(testArray[i + 3], 10);
+        testArray[opParameter3] = processOpcode(opInstruction, opParameter1, opParameter2);
+      } else {
+        haltComputer = true;
+      }
+    }
+
+    if (testArray[0] === 19690720) {
+      computationComplete = true;
+      // eslint-disable-next-line no-console
+      console.log(`Noun (${noun}) + Verb (${verb}) Found!`);
+    }
+  }
+}
